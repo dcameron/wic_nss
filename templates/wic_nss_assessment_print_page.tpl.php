954ef50ffa800bd8bc03c5c45b2f70bedf9fd550
David <david.a.cameron@sbcglobal.net>
2013-02-18 17:34:31 -0600

    Added assessment print page.

---
+++ templates/wic_nss_assessment_print_page.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Display all submissions in a self-assessment.
+ *
+ * Available variables:
+ * - $submission_content: The contents of the webform submissions.
+ */
+?>
+
+<div class="webform-submission">
+  <?php print render($submission_content); ?>
+</div>
