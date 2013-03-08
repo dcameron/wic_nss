0fc86a54c54caf78482643758564e48cd49387b9
David <david.a.cameron@sbcglobal.net>
2013-03-07 18:33:10 -0600

    Created the basic dashboard page.

---
+++ templates/wic_nss_assessment_menu.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Display a menu of a self-assessment.
+ *
+ * Available variables:
+ * - $elements: An array of links.
+ */
+?>
+
+<div class="assessment-menu">
+  <?php print render($elements); ?>
+</div>
