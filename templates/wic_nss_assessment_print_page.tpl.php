11e781ed042d22173e265bf5a795c55f6593ad05
David <david.a.cameron@sbcglobal.net>
2013-03-11 13:28:09 -0500

    Moved print page HTML formatting from preproecess function to template.

--- templates/wic_nss_assessment_print_page.tpl.php
+++ templates/wic_nss_assessment_print_page.tpl.php
@@ -5,10 +5,22 @@
  * Display all submissions in a self-assessment.
  *
  * Available variables:
- * - $submission_content: The contents of the webform submissions.
+ * - $bid: The nid of the assessment book node.
+ * - $submission_content: An array containing the following keys:
+ *   - title: The title of the submitted webform.
+ *   - content: The rendered content of the submission.
  */
 ?>
 
-<div class="webform-submission">
-  <?php print render($submission_content); ?>
-</div>
+<?php if (empty($submission_content)): ?>
+  <p><?php print t("You haven't submitted any forms for this self-assessment yet."); ?></p>
+  <p><?php print l(t('Start filling out forms.'), 'node/' . $bid); ?></p>
+
+<?php else: ?>
+  <?php foreach ($submission_content as $submission): ?>
+    <div class="wic-nss-webform-submission">
+      <h2><?php print $submission['title']; ?></h2>
+      <?php print render($submission['content']); ?>
+    </div>
+  <?php endforeach; ?>
+<?php endif; ?>
