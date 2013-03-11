8d5bbdabfd6a83d8ce8f270503e0ecc1f1cc3da4
David <david.a.cameron@sbcglobal.net>
2013-03-11 17:10:00 -0500

    Added a jump menu ToC to the print page.

--- templates/wic_nss_assessment_print_page.tpl.php
+++ templates/wic_nss_assessment_print_page.tpl.php
@@ -6,6 +6,7 @@
  *
  * Available variables:
  * - $bid: The nid of the assessment book node.
+ * - $table_of_contents: A jump menu list of links to the submissions.
  * - $submission_content: An array containing the following keys:
  *   - title: The title of the submitted webform.
  *   - content: The rendered content of the submission.
@@ -17,8 +18,12 @@
   <p><?php print l(t('Start filling out forms.'), 'node/' . $bid); ?></p>
 
 <?php else: ?>
+  <div class="wic-nss-submissions-toc">
+    <h2>Table of contents</h2>
+    <?php print $table_of_contents; ?>
+  </div>
   <?php foreach ($submission_content as $submission): ?>
-    <div class="wic-nss-webform-submission">
+    <div id="<?php print 'submission-' . $submission['sid']; ?>" class="wic-nss-webform-submission">
       <h2><?php print $submission['title']; ?></h2>
       <?php print render($submission['content']); ?>
     </div>
