aaea1d717f7d9a050b345b99e5bcf6e9820ddf4a
David <david.a.cameron@sbcglobal.net>
2013-03-14 19:07:10 -0500

    View report and print PDF pages now pull node list from same function and in hierarchical order.

--- templates/wic_nss_assessment_view_page.tpl.php
+++ templates/wic_nss_assessment_view_page.tpl.php
@@ -7,13 +7,11 @@
  * Available variables:
  * - $bid: The nid of the assessment book node.
  * - $table_of_contents: A jump menu list of links to the submissions.
- * - $submission_content: An array containing the following keys:
- *   - title: The title of the submitted webform.
- *   - content: The rendered content of the submission.
+ * - $content: An array containing content from nodes in the assessment.
  */
 ?>
 
-<?php if (empty($submission_content)): ?>
+<?php if (empty($content)): ?>
   <p><?php print t("You haven't submitted any forms for this self-assessment yet."); ?></p>
   <p><?php print l(t('Start filling out forms.'), 'node/' . $bid); ?></p>
 
@@ -22,11 +20,13 @@
     <h2>Table of contents</h2>
     <?php print $table_of_contents; ?>
   </div>
-  <?php foreach ($submission_content as $submission): ?>
-    <div id="<?php print 'submission-' . $submission['sid']; ?>" class="wic-nss-webform-submission">
-      <h2><?php print $submission['title']; ?></h2>
-      <?php print render($submission['body']); ?>
-      <?php print render($submission['content']); ?>
+  <?php foreach ($content as $nid => $node_content): ?>
+    <div id="<?php print 'node-' . $nid; ?>" class="wic-nss-node-display">
+      <h2><?php print $node_content['title']; ?></h2>
+      <?php print render($node_content['body']); ?>
+      <?php if (!empty($node_content['submission'])): ?>
+        <?php print render($node_content['submission']); ?>
+      <?php endif; ?>
     </div>
   <?php endforeach; ?>
 <?php endif; ?>
