1763621a7d6bde62e4cf60809fe3a39e2e9b733c
David <david.a.cameron@sbcglobal.net>
2013-03-15 13:41:09 -0500

    Filtered childless content from the assessment reports.

--- templates/wic_nss_assessment_view_page.tpl.php
+++ templates/wic_nss_assessment_view_page.tpl.php
@@ -11,22 +11,16 @@
  */
 ?>
 
-<?php if (empty($content)): ?>
-  <p><?php print t("You haven't submitted any forms for this self-assessment yet."); ?></p>
-  <p><?php print l(t('Start filling out forms.'), 'node/' . $bid); ?></p>
-
-<?php else: ?>
-  <div class="wic-nss-submissions-toc">
-    <h2>Table of contents</h2>
-    <?php print $table_of_contents; ?>
+<div class="wic-nss-submissions-toc">
+  <h2>Table of contents</h2>
+  <?php print $table_of_contents; ?>
+</div>
+<?php foreach ($content as $nid => $node_content): ?>
+  <div id="<?php print 'node-' . $nid; ?>" class="wic-nss-node-display">
+    <h2><?php print $node_content['title']; ?></h2>
+    <?php print render($node_content['body']); ?>
+    <?php if (!empty($node_content['submission'])): ?>
+      <?php print render($node_content['submission']); ?>
+    <?php endif; ?>
   </div>
-  <?php foreach ($content as $nid => $node_content): ?>
-    <div id="<?php print 'node-' . $nid; ?>" class="wic-nss-node-display">
-      <h2><?php print $node_content['title']; ?></h2>
-      <?php print render($node_content['body']); ?>
-      <?php if (!empty($node_content['submission'])): ?>
-        <?php print render($node_content['submission']); ?>
-      <?php endif; ?>
-    </div>
-  <?php endforeach; ?>
-<?php endif; ?>
+<?php endforeach; ?>
