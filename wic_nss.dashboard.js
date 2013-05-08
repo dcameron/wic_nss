a006205ec3fb86f1793ec3a70d584a60fe730d88
David <david.a.cameron@sbcglobal.net>
2013-05-08 14:42:08 -0500

    Altered dashboard javascript to use Drupal behaviors.

--- wic_nss.dashboard.js
+++ wic_nss.dashboard.js
@@ -5,27 +5,28 @@
 
 (function ($) {
 
-  $(document).ready(function () {
-    $('div.assessment-menu li li.expanded').each(function() {
-      $(this).removeClass('expanded');
-      $(this).addClass('collapsed');
-      $(this).find('a:first').after(' <span class="expansion-target">(click here to show sub-forms)</span>');
-      $(this).find('ul.menu').hide();
-    });
-    $('span.expansion-target').click(function() {
-      if ($(this).parent().hasClass('collapsed')) {
-        $(this).html('(click here to hide sub-forms)');
-        $(this).parent().removeClass('collapsed');
-        $(this).parent().addClass('expanded');
-      }
-      else if ($(this).parent().hasClass('expanded')) {
-        $(this).html('(click here to show sub-forms)');
-        $(this).parent().removeClass('expanded');
-        $(this).parent().addClass('collapsed');
-      }
-      $(this).parent().find('ul.menu:first').slideToggle('fast');
+  Drupal.behaviors.wic_nss = {
+    attach: function (context, settings) {
+      $('div.assessment-menu li li.expanded').each(function() {
+        $(this).removeClass('expanded');
+        $(this).addClass('collapsed');
+        $(this).find('a:first').after(' <span class="expansion-target">(click here to show sub-forms)</span>');
+        $(this).find('ul.menu').hide();
+      });
+      $('span.expansion-target').click(function() {
+        if ($(this).parent().hasClass('collapsed')) {
+          $(this).html('(click here to hide sub-forms)');
+          $(this).parent().removeClass('collapsed');
+          $(this).parent().addClass('expanded');
+        }
+        else if ($(this).parent().hasClass('expanded')) {
+          $(this).html('(click here to show sub-forms)');
+          $(this).parent().removeClass('expanded');
+          $(this).parent().addClass('collapsed');
+        }
+        $(this).parent().find('ul.menu:first').slideToggle('fast');
+      });
     }
-    );
-  });
+  };
 
 })(jQuery);
